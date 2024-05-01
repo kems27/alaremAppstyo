09f29615871178675adeb819f80af8ffc700e96b
Prasetyo Ludiono <kembon1109@gmail.com>
2024-05-01 08:49:38 +0700

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+//initial References
+let timerRef = document.querySelector(".time-display");
+const hourInput =document.getElementById("hourInput");
+const minuteInput =document.getElementById("minuteInput");
+const activeAlarms=document.querySelector(."activeAlarms");
+const setAlarm=document.getElementById("set");
+let
+let AlarmsArray=new Audio(".")
